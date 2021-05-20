75454e9fd7826640db5dcd6e21511107aa833b7e
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2021-05-20 22:14:20 +0300

    Initial commit

---
+++ NixLesson1/ClassLibrary1/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class User
+    {
+        string _name;
+        int _age = 18;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Age { get { return _age; } set { if (value >= 0) _age = value; } }
+
+        public User() : this("User") { }
+
+        public User(string name) : this(name, 19)
+        {
+            Name = name;
+        }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"User \'{Name}\' is {Age} years old!";
+        }
+
+        public void isAdult()
+        {
+            Console.WriteLine(Age >= 18 ? $"User \'{Name}\' is adult!" : $"User \'{Name}\' is not adult!");
+        }
+    }
+}
---
+++ NixLesson1/Project2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class User
+    {
+    }
+
+}
